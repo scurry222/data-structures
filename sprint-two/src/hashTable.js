5e524813add65e3647955fbab7858f37aee0727f
scurry222 <scurry222@gmail.com>
2020-11-19 17:54:07 -0800

    Add hashtable functions (tests pass)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index)) {
+    this._storage.get(index)[k] = v;
+  } else {
+    const pair = {};
+    pair[k] = v;
+    this._storage.set(index, pair);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  if (bucket) {
+    return bucket[k];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
